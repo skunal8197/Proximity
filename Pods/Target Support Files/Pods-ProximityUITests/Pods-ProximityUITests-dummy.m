26e074e47bf6ad7458a7449a4fc7692217797301
skunal8197 <skunal8197@gmail.com>
2018-02-24 18:23:09 -0600

    initial commit

---
+++ Pods/Target Support Files/Pods-ProximityUITests/Pods-ProximityUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProximityUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ProximityUITests
+@end
---
+++ Proximity/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Proximity
+//
+//  Created by Kunal Shah on 2/24/18.
+//  Copyright © 2018 Proximity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//MARK: Constants and Enums
+
+
